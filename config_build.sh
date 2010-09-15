a2e7d0ff8751c6bf754bd5db9a9cd43b02ad7826
Chao ZHOU <chao@zhou.fr>
2010-09-15 21:32:23 +0800

    Fixed the config_build bug.

--- config_build.sh
+++ config_build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Build config for build.sh
 APP_NAME="lasuli"
-CHROME_PROVIDERS=
+CHROME_PROVIDERS="content locale skin"
 CLEAN_UP=1
 ROOT_FILES=
-ROOT_DIRS="content locale skin modules"
+ROOT_DIRS="modules"
 BEFORE_BUILD=
 AFTER_BUILD=
