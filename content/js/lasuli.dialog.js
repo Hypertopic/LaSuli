6d1b40ad42e0d8dc0893998fcd9cf8b2c0ab779c
Chao ZHOU <chao@zhou.fr>
2010-09-21 16:09:03 +0800

    Updated the options dialog for multiple server.
    TODO: check each sever when OK is clicked.

---
+++ content/js/lasuli.dialog.js
@@ -0,0 +1,44 @@
+function onOK()
+{
+  var baseUrl = document.getElementById('txtBaseUrl').value;
+	var user    = document.getElementById('txtUser').value;
+	var pass    = document.getElementById('txtPass').value;
+
+	var prompts = Cc["@mozilla.org/embedcomp/prompt-service;1"]
+                        .getService(Ci.nsIPromptService);
+
+  if (baseUrl == "")
+  {
+    prompts.alert(window, _('Error'), _('options.dialog.error.urlisnull'));
+    return false;
+	}
+	if (user == '')
+	{
+    prompts.alert(window, _('Error'), _('options.dialog.error.userisnull'));
+    return false;
+	}
+  if(baseUrl.substr(-1) != '/')
+    baseUrl = baseUrl + '/';
+
+  var server  = window.arguments[0];
+  server.url = baseUrl;
+  server.user = user;
+  server.pass = pass;
+	return true;
+}
+
+function onLoad()
+{
+  var server  = window.arguments[0];
+  document.getElementById('txtBaseUrl').value = (server && server.url) ? server.url : '';
+  document.getElementById('txtUser').value = (server && server.user) ? server.user : '';
+  document.getElementById('txtPass').value = (server && server.pass) ? server.pass : '';
+  var title = (server && server.url) ? _('options.dialog.modify.title') : _('options.dialog.add.title');
+  document.getElementById('lasuli-options-dialog').setAttribute('title', title);
+  document.getElementById('options.dialog.header').setAttribute('label', _('options.dialog.header'));
+  document.getElementById('options.dialog.url').value = _('options.dialog.url');
+  document.getElementById('options.dialog.user').value = _('options.dialog.user');
+  document.getElementById('options.dialog.pass').value = _('options.dialog.pass');
+  document.getElementById('options.dialog.accept').setAttribute('label', _('options.dialog.accept'));
+  document.getElementById('options.dialog.cancel').setAttribute('label', _('options.dialog.cancel'));
+}
--- content/js/lasuli.options.js
+++ content/js/lasuli.options.js
@@ -23,23 +23,36 @@ lasuli.options = {
 	  btnDelete.setAttribute('label', _('options.server.delete'));
 	  btnDefault.setAttribute('label', _('options.server.default'));
 
-	  this.servers = new Array({'url': 'http://hypertopic.couchone.com/argos/', 'user':'chao@zhou.fr', 'pass':'', 'default': true},
+	  /*this.servers = new Array({'url': 'http://hypertopic.couchone.com/argos/', 'user':'chao@zhou.fr', 'pass':'', 'default': true},
 	                          {'url': 'http://lasuli.couchone.com/argos/', 'user':'chao@hypertopic.org', 'pass':''},
 	                          {'url': 'http://lasuli.couchone.com/lasuli/', 'user':'user@hypertopic.org', 'pass':''});
-	  Preferences.set("extensions.lasuli.setting",JSON.stringify(this.servers));
+	  Preferences.set("extensions.lasuli.setting",JSON.stringify(this.servers));*/
     //Load setting from preferences
     this.servers = Preferences.get("extensions.lasuli.setting",JSON.stringify(new Array()));
     if(typeof(this.servers) == "string") this.servers = JSON.parse(this.servers);
-    var lists = document.getElementById('options-list');
+    this.listServers();
+    window.sizeToContent();
+  },
+
+  listServers : function(){
+    var logger = Log4Moz.repository.getLogger("lasuli.options.listServers");
+    var list = document.getElementById('options-list');
+    for(var i=0, node; node = list.childNodes[i]; i++){
+      logger.debug(node.tagName);
+      if(node.tagName == 'listitem'){
+        list.removeChild(node);
+        i--;
+      }
+    }
+
     //Display settings
     for(var i=0, server; server = this.servers[i]; i++)
     {
       var listitem = document.createElement('listitem');
       var listcell = document.createElement('listcell');
+      listcell.setAttribute("class", "listcell-iconic");
       if(server.default === true)
-        listcell.setAttribute('label', 'D');
-      else
-        listcell.setAttribute('label', ' ');
+        listcell.setAttribute("image", "css/icons/favorite.png");
       listitem.appendChild(listcell);
 
       listcell = document.createElement('listcell');
@@ -50,14 +63,27 @@ lasuli.options = {
       listcell.setAttribute('label', server.url);
       listitem.appendChild(listcell);
 
-      lists.appendChild(listitem);
+      list.appendChild(listitem);
     }
   },
 
   okay : function(){
     var logger = Log4Moz.repository.getLogger("lasuli.options.okay");
+    //TODO check every server url and test it if it's accessible.
     Preferences.set("extensions.lasuli.setting",JSON.stringify(this.servers));
     return true;
+  },
+
+  getSelectedItem : function(){
+    var list = document.getElementById('options-list');
+    var i = list.selectedIndex;
+    var item = list.selectedItem;
+    var cells = item.childNodes;
+    var server = {};
+    server.default = cells[0].hasAttribute('image');
+    server.user = (cells[1].getAttribute('label')) ? cells[1].getAttribute('label') : '';
+    server.url = (cells[2].getAttribute('label')) ? cells[2].getAttribute('label') : '';
+    return {"list": list, "item": item, "server": server, "selectedIndex": i};
   },
 
   register: function(){
@@ -74,7 +100,85 @@ lasuli.options = {
 
   doCreate : function(){
     var server = {};
-    window.open('setting.xul');
+    var dialog = openDialog('dialog.xul', 'server-dialog', 'modal', server);
+    dialog.sizeToContent();
+    if(server.url && server.user){
+      for(i=0; i < this.servers.length; i++)
+        if(this.servers[i].user == server.user &&
+            this.servers[i].url == server.url)
+          return;
+      this.servers.push(server);
+      this.listServers();
+    }
+  },
+
+  doModify : function(){
+    var logger = Log4Moz.repository.getLogger("lasuli.options.doModify");
+    var server = {}, i;
+    var obj = this.getSelectedItem();
+    logger.debug(obj.server);
+    for(i=0; i < this.servers.length; i++)
+    {
+      logger.debug(this.servers[i]);
+      if(this.servers[i].user == obj.server.user &&
+          this.servers[i].url == obj.server.url)
+      {
+        server = this.servers[i];
+        break;
+      }
+    }
+    var dialog = openDialog('dialog.xul', 'server-dialog', 'modal', server);
+    dialog.sizeToContent();
+    if(server.url && server.user){
+      this.servers[i].url = server.url;
+      this.servers[i].user = server.user;
+      this.servers[i].pass = server.pass;
+      this.listServers();
+    }
+  },
+
+  doDelete : function(){
+    var logger = Log4Moz.repository.getLogger("lasuli.options.doDelete");
+    var obj = this.getSelectedItem();
+    for(var i=0; i < this.servers.length; i++)
+      if(this.servers[i].user == obj.server.user &&
+          this.servers[i].url == obj.server.url)
+      {
+          this.servers.splice(i, 1);
+          break;
+      }
+    logger.debug(this.servers);
+    this.listServers();
+    this.doSetButtonStatus();
+  },
+
+  doSetDefault : function(){
+    var obj = this.getSelectedItem();
+    for(var i=0; i < this.servers.length; i++)
+      if(this.servers[i].user == obj.server.user &&
+          this.servers[i].url == obj.server.url)
+        this.servers[i].default = true;
+      else
+        this.servers[i].default = false;
+
+    this.listServers();
+  },
+
+  doSetButtonStatus : function(){
+    var list = document.getElementById('options-list');
+    var btnModify     = document.getElementById('button-modify');
+	  var btnAdd        = document.getElementById('button-add');
+	  var btnDelete     = document.getElementById('button-delete');
+	  var btnDefault    = document.getElementById('button-default');
+	  btnModify.setAttribute('disabled', true);
+	  btnDelete.setAttribute('disabled', true);
+	  btnDefault.setAttribute('disabled', true);
+    if(list.selectedCount > 0){
+      btnModify.removeAttribute('disabled');
+      btnDelete.removeAttribute('disabled');
+      if(!list.selectedItem.firstChild.hasAttribute('image'))
+        btnDefault.removeAttribute('disabled');
+    }
   }
 }
 
