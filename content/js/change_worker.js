c73cfca6322d9df298a37daa0e86e6a527220833
Chao ZHOU <chao@zhou.fr>
2010-12-15 15:59:08 +0800

    Fixed the changes watcher worker bug.

--- content/js/change_worker.js
+++ content/js/change_worker.js
@@ -1,40 +1,61 @@
 function fetch(){
   if(!changeWorker.run) return;
-
+  //dump("Interval:" + ((new Date()).getTime() - changeWorker.lastTime) + "\n");
+  //changeWorker.lastTime = (new Date()).getTime();
   for(var server in changeWorker.servers)
   {
     var baseUrl = changeWorker.servers[server].baseUrl;
     var changeUrl = baseUrl + "_changes"
-    if(changeWorker.sequences[server])
-      changeUrl += "?since=" + changeWorker.sequences[server];
-
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.open("GET", changeUrl, false);
-    httpRequest.onload = function infoReceived()
+    if(!changeWorker.sequences[server])
     {
-      var output = httpRequest.responseText;
-      if (output) {
-        var result = JSON.parse(output.trim());
-        var seq = -1;
-        try{
-          seq = parseInt(result.last_seq);
-        }catch(e){}
-        //postMessage(changeWorker.sequences[server] + "," + seq);
-        if(!changeWorker.sequences[server] || changeWorker.sequences[server] < seq)
+      var seq = getLastSeq(changeUrl);
+      //dump(seq);
+      if(seq < 0)
+      {
+        changeWorker.timeoutID = setTimeout("fetch()", 5000);
+        return false;
+      }
+      changeWorker.sequences[server] = seq;
+    }
+    changeUrl += "?since=" + changeWorker.sequences[server];
+
+    var req = new XMLHttpRequest();
+    req.open("GET", changeUrl, true);
+    req.onload = function()
+    {
+      var seq = -1;
+      try{
+        var result = JSON.parse(req.responseText.trim());
+        if(!changeWorker.sequences[server] || changeWorker.sequences[server] < result.last_seq)
         {
-          changeWorker.sequences[server] = seq;
+          dump(server);
+          changeWorker.sequences[server] = result.last_seq;
           postMessage(server);
         }
-      }
-      httpRequest = null;
+      }catch(e){}
     }
-    httpRequest.send(null);
-    //postMessage('fetch:' + changeUrl);
+    req.send(null);
   }
-  setTimeout("fetch()", 5000);
+  changeWorker.timeoutID = setTimeout("fetch()", 5000);
+}
+
+function getLastSeq(url){
+  //dump(url);
+  var req = new XMLHttpRequest();
+  req.open("GET", url, false);
+  req.send(null);
+  if(req.status == "200" || req.status == "304")
+    try{
+      var result = JSON.parse(req.responseText.trim());
+      return result.last_seq;
+    }catch(e){
+      dump("Get last sequence no error");
+    }
+  return -1;
 }
 
 var changeWorker = {
+  lastTime: (new Date()).getTime(),
   servers : null,
   run : true,
   sequences: {},
@@ -43,11 +64,18 @@ var changeWorker = {
     this.run = true;
     this.servers = HtServers;
     this.sequences = {};
+    dump("Listner start\n");
     fetch();
   },
 
   shutdown: function(){
     this.run = false;
+    if(typeof this.timeoutID == "number") {
+      dump("clear timeout\n");
+      clearTimeout(this.timeoutID);
+      delete this.timeoutID;
+    }
+    dump("Listner shutdown\n");
   }
 }
 
