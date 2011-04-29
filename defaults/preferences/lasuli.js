285264e963a557f563e485f3db8a74ce0464f762
Chao ZHOU <chao@zhou.fr>
2011-04-29 20:27:16 +0800

    Work on configuration panel.

---
+++ defaults/preferences/lasuli.js
@@ -0,0 +1,1 @@
+pref("extensions.lasuli.setting", '[{"default": true,"url": "http://argos2.hypertopic.org", "user": "guest", "pass": "guest"}, {"url": "http://cassandre.hypertopic.org", "user": "guest", "pass": "guest"}]');
