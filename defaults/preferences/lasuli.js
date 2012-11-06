67610f29bb373260041264726d5479b251ef5739
Aurelien Benel <aurelien.benel@utt.fr>
2012-11-06 11:56:28 +0100

    FIX: Configuration made easier.

--- defaults/preferences/lasuli.js
+++ defaults/preferences/lasuli.js
@@ -1,3 +1,3 @@
-pref("extensions.lasuli.setting", '[{"default": true,"url": "http://argos2.hypertopic.org", "user": "guest", "pass": "guest"}, {"url": "http://cassandre.hypertopic.org", "user": "guest", "pass": "guest"}]');
+pref("extensions.lasuli.setting", '[{"default": true,"url": "http://argos2.hypertopic.org/", "user": "prenom.nom", "pass": ""}]');
 pref("extensions.lasuli.log.level", "Info");
 pref("extensions.lasuli.cache", true);
